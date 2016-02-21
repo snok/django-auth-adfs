d675308efb3f1c1766f628750c9f31482c4c4f0a
Joris Beckers <joris.beckers@gmail.com>
2016-02-21 18:05:35 +0100

    Fix for Django 1.7 tests

--- tests/settings.py
+++ tests/settings.py
@@ -33,7 +33,6 @@ TEMPLATE_DIRS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
