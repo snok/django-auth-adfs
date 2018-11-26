40a0a52dc8ca25cff4ed77e3aae681dba6ff1b31
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2018-11-26 12:48:37 +0100

    Fixing issue mentioned in #54
    In order to fix remaining of #54. 
    
    ```py
    DEBUG 2018-11-26 12:05:07,165 django_auth_adfs Attribute 'testuser' for user 'first_name' was set to 'Test'.
    DEBUG 2018-11-26 12:05:07,165 django_auth_adfs Attribute 'testuser' for user 'last_name' was set to 'Some Lastname'.
    DEBUG 2018-11-26 12:05:07,166 django_auth_adfs Attribute 'testuser' for user 'email' was set to 'test@testuser.com'.
    Internal Server Error: /api/api/test/
    Traceback (most recent call last):
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\core\handlers\exception.py", line 34, in inner
        response = get_response(request)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\core\handlers\base.py", line 126, in _get_response
        response = self.process_exception_by_middleware(e, request)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\core\handlers\base.py", line 124, in _get_response
        response = wrapped_callback(request, *callback_args, **callback_kwargs)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\views\decorators\csrf.py", line 54, in wrapped_view
        return view_func(*args, **kwargs)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\views\generic\base.py", line 68, in view
        return self.dispatch(request, *args, **kwargs)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\views.py", line 495, in dispatch
        response = self.handle_exception(exc)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\views.py", line 455, in handle_exception
        self.raise_uncaught_exception(exc)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\views.py", line 483, in dispatch
        self.initial(request, *args, **kwargs)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\views.py", line 400, in initial
        self.perform_authentication(request)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\views.py", line 326, in perform_authentication
        request.user
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\request.py", line 223, in user
        self._authenticate()
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\rest_framework\request.py", line 376, in _authenticate
        user_auth_tuple = authenticator.authenticate(self)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django_auth_adfs\rest_framework.py", line 38, in authenticate
        user = authenticate(access_token=auth[1])
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django\contrib\auth\__init__.py", line 73, in authenticate
        user = backend.authenticate(request, **credentials)
      File "C:\Users\AppData\Local\Continuum\anaconda3\envs\oauthminimal\lib\site-packages\django_auth_adfs\backend.py", line 77, in authenticate
        adfs_response=adfs_response
    UnboundLocalError: local variable 'adfs_response' referenced before assignment
    [26/Nov/2018 12:05:07] "GET /api/api/test/ HTTP/1.1" 500 25313
    ```

--- django_auth_adfs/backend.py
+++ django_auth_adfs/backend.py
@@ -74,7 +74,7 @@ class AdfsBackend(ModelBackend):
             sender=self,
             user=user,
             claims=claims,
-            adfs_response=adfs_response
+            adfs_response=access_token
         )
 
         user.full_clean()
