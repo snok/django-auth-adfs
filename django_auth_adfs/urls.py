f0cd566bbdc260d327a57ad3af5977f5963ff81c
xavirg <63903895+xavirg@users.noreply.github.com>
2021-01-22 08:46:26 +0100

    Remove deprecated methods from urls.py
    url() is deprecated since Django version 3.1.

--- django_auth_adfs/urls.py
+++ django_auth_adfs/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import re_path
 
 from django_auth_adfs import views
 
 app_name = "django_auth_adfs"
 
 urlpatterns = [
-    url(r'^callback$', views.OAuth2CallbackView.as_view(), name='callback'),
-    url(r'^login$', views.OAuth2LoginView.as_view(), name='login'),
-    url(r'^login_no_sso$', views.OAuth2LoginNoSSOView.as_view(), name='login-no-sso'),
-    url(r'^login_force_mfa$', views.OAuth2LoginForceMFA.as_view(), name='login-force-mfa'),
-    url(r'^logout$', views.OAuth2LogoutView.as_view(), name='logout'),
+    re_path(r'^callback$', views.OAuth2CallbackView.as_view(), name='callback'),
+    re_path(r'^login$', views.OAuth2LoginView.as_view(), name='login'),
+    re_path(r'^login_no_sso$', views.OAuth2LoginNoSSOView.as_view(), name='login-no-sso'),
+    re_path(r'^login_force_mfa$', views.OAuth2LoginForceMFA.as_view(), name='login-force-mfa'),
+    re_path(r'^logout$', views.OAuth2LogoutView.as_view(), name='logout'),
 ]
