abdee8941d2b850f725be72d25d02be1fe2544f7
WardCornettePrivate <69187379+WardCornettePrivate@users.noreply.github.com>
2022-06-08 16:25:19 +0200

    PR: Enhanced tests

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.IntegerField(blank=True, null=True)
